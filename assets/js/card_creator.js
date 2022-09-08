6816b247f16193b57c6f42a848037cadbe736c3f
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-09-08 06:17:21 -0400

    Create card_creator.js

---
+++ assets/js/card_creator.js
@@ -0,0 +1,21 @@
+
+
+
+function encode(){
+  var res = LZString.compressToBase64($('#input').val());
+  var phone = LZString.compressToBase64($('#phone').val());
+  var whatsappApiUrl = 'https://marketingpip.github.io/Send-A-Card/card?text=';
+  res = whatsappApiUrl + res + '&name=' + phone;
+  $('#output').text(res);
+  $('#invia').attr('href', res);
+}
+
+// Encode on load.
+encode();
+// Encode while writing.
+$('#input').on('keyup', function(){
+  encode();
+});
+$('#phone').on('keyup', function(){
+  encode();
+});
