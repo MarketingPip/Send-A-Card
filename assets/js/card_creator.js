0566642e008051eaa8e019e27d0ada8b1a0de0c3
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-09-08 06:35:20 -0400

    Update card_creator.js

--- assets/js/card_creator.js
+++ assets/js/card_creator.js
@@ -5,7 +5,7 @@ function encode(){
   var res = LZString.compressToBase64($('#input').val());
   var phone = LZString.compressToBase64($('#phone').val());
   var whatsappApiUrl = 'https://marketingpip.github.io/Send-A-Card/card?text=';
-  res = whatsappApiUrl + res + '&name=' + phone;
+  res = encodeURIComponent(whatsappApiUrl + res);
   $('#output').text(res);
   $('#invia').attr('href', res);
 }
