08c705c5d65940ee9d9ab7e93abe82e83cef1dae
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-09-08 23:18:17 -0400

    Update card_creator.js

--- assets/js/card_creator.js
+++ assets/js/card_creator.js
@@ -1,10 +1,10 @@
 
+
+
+function encode(){
   var res = encodeURIComponent(LZString.compressToBase64($('#input').val()));
   var phone = encodeURIComponent(LZString.compressToBase64($('#phone').val()));
   var whatsappApiUrl = 'https://marketingpip.github.io/Send-A-Card/card?text=';
-
-function encode(){
-
   res = whatsappApiUrl + res + '&name=' + phone;
   $('#output').text(res);
   $('#invia').attr('href', res);
@@ -21,15 +21,4 @@ $('#input').on('keyup', function(){
 $('#phone').on('keyup', function(){
   encode();
 });
-
-
-var script = document.createElement('script');
-script.onload = function () {
-    //do stuff with the script
-  console.log("ds")
-  $("select").imagepicker()
-};
-script.src = "https://cdnjs.cloudflare.com/ajax/libs/image-picker/0.3.1/image-picker.min.js";
-
-document.head.appendChild(script); //or something of the likes
 
